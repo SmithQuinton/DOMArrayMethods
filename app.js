a3f9e40218882fec7989611b5c62edc0bbf62926
Quinton Smith <smith.quinton42@gmail.com>
2020-12-07 23:47:18 -0500

    calculate wealth - reduce()

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const addUserBtn =document.getElementById('add-user');
 const doubleBtn =document.getElementById('double');
 const showMillionairesBtn =document.getElementById('show-millionaires');
 const sortBtn =document.getElementById('sort');
-const calcuateWealthBtn =document.getElementById('calculate-wealth');
+const calculateWealthBtn =document.getElementById('calculate-wealth');
 
 
 let data = [];
@@ -54,6 +54,15 @@ function showMillionaires() {
     updateDOM();
 }
 
+// Calculate total Wealth
+function calculateWealth() {
+    const wealth = data.reduce((acc,user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 // Add new obj to data arr
 function addData(obj) {
     data.push(obj);
@@ -84,3 +93,4 @@ addUserBtn.addEventListener('click', getRandomUser)
 doubleBtn.addEventListener('click', doubleMoney)
 sortBtn.addEventListener('click', sortByRichest)
 showMillionairesBtn.addEventListener('click', showMillionaires)
+calculateWealthBtn.addEventListener('click', calculateWealth)
